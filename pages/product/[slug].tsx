c166f951822feb9b2cdecb1c780fa1197ffa917a
elkadye <73303777+elkadye@users.noreply.github.com>
2022-05-12 17:57:31 +0200

    single product

---
+++ pages/product/[slug].tsx
@@ -0,0 +1,203 @@
+import { RadioGroup } from '@headlessui/react'
+import { StarIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
+import Layout from 'components/layout'
+import { classNames } from 'lib'
+
+const oldProduct = {
+  name: 'Basic Tee 6-Pack',
+  price: '$192',
+  href: '#',
+  images: [
+    {
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
+      alt: 'Two each of gray, white, and black shirts laying flat.',
+    },
+    {
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
+      alt: 'Model wearing plain black basic tee.',
+    },
+    {
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
+      alt: 'Model wearing plain gray basic tee.',
+    },
+    {
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
+      alt: 'Model wearing plain white basic tee.',
+    },
+  ],
+  colors: [
+    { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+    { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+    { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+  ],
+  sizes: [
+    { name: 'XXS', inStock: false },
+    { name: 'XS', inStock: true },
+    { name: 'S', inStock: true },
+    { name: 'M', inStock: true },
+    { name: 'L', inStock: true },
+    { name: 'XL', inStock: true },
+    { name: '2XL', inStock: true },
+    { name: '3XL', inStock: true },
+  ],
+  description:
+    'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+  highlights: [
+    'Hand cut and sewn locally',
+    'Dyed with our proprietary colors',
+    'Pre-washed & pre-shrunk',
+    'Ultra-soft 100% cotton',
+  ],
+  details:
+    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+}
+const reviews = {
+  href: '#',
+  average: 4,
+  totalCount: 117,
+  featured: [
+    {
+      id: 1,
+      title: 'This is the best white t-shirt out there',
+      rating: 5,
+      content: `
+        <p>I've searched my entire life for a t-shirt that reflects every color in the visible spectrum. Scientists said it couldn't be done, but when I look at this shirt, I see white light bouncing right back into my eyes. Incredible!</p>
+      `,
+      author: 'Mark Edwards',
+      avatarSrc:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixqx=oilqXxSqey&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      id: 2,
+      title: 'Adds the perfect variety to my wardrobe',
+      rating: 4,
+      content: `
+        <p>I used to be one of those unbearable minimalists who only wore the same black v-necks every day. Now, I have expanded my wardrobe with three new crewneck options! Leaving off one star only because I wish the heather gray was more gray.</p>
+      `,
+      author: 'Blake Reid',
+      avatarSrc:
+        'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80',
+    },
+    {
+      id: 3,
+      title: 'All good things come in 6-Packs',
+      rating: 5,
+      content: `
+        <p>Tasty beverages, strong abs that will never be seen due to aforementioned tasty beverages, and these Basic Tees!</p>
+      `,
+      author: 'Ben Russel',
+      avatarSrc:
+        'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  ],
+}
+import useSWR from 'swr'
+import axios from 'axios'
+import { RootProduct } from 'types'
+
+import { useRouter } from 'next/router'
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data)
+
+export default function ProductPage() {
+  const router = useRouter()
+  const { slug } = router.query
+  if (!slug) {
+    return <div> waiting for slug.. </div>
+  }
+  const { data, error } = useSWR('/api/' + slug, fetcher)
+  if (!data) {
+    return <div> waiting for data</div>
+  }
+  const product: RootProduct = data
+  console.log(slug)
+  console.log(data)
+  console.log(product.product_images[0].imageSrc)
+  // const [open, setOpen] = useState(false)
+  // const [selectedColor, setSelectedColor] = useState(product.colors[0])
+  // const [selectedSize, setSelectedSize] = useState(product.sizes[2])
+function addCartItem(){
+  const changeQty=1
+  const payload= {
+    ...product,
+    changeQty
+  }
+console.log(payload)
+}
+  return (
+    <Layout>
+      <main className="pt-10 sm:pt-8 md:pt-0">
+        {/* Image gallery */}
+        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+          {product.product_images.map((image) => (
+            <div className="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
+              <img key={image.ID} src={image.imageSrc} alt={image.imageAlt} />
+
+              {/* <img
+              src={product.product_images[0].imageSrc}
+              alt={product.product_images[0].imageAlt}
+              className="h-full w-full object-cover object-center"
+            /> */}
+            </div>
+          ))}
+        </div>
+
+        {/* Product info */}
+        <div className="mx-auto max-w-2xl px-4 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:py-16">
+          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+              {product.name}
+            </h1>
+          </div>
+
+          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
+            {/* Description and details */}
+            <div>
+              <h3 className="sr-only">Description</h3>
+
+              <div className="space-y-6">
+                <p className="text-base text-gray-900">{product.description}</p>
+              </div>
+            </div>
+
+            <section aria-labelledby="shipping-heading" className="mt-10">
+              <h2
+                id="shipping-heading"
+                className="text-sm font-medium text-gray-900"
+              >
+                Details
+              </h2>
+
+              <div className="mt-4 space-y-6">
+                <p className="text-sm text-gray-600">{product.details}</p>
+              </div>
+            </section>
+          </div>
+
+          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+            {/* Place Item in cart */}
+            <section>
+              <div className="mt-10">
+                <h3 className="text-sm font-medium text-gray-900">
+                  Highlights
+                </h3>
+                <div className="mt-4">
+                  {/* <form className="mt-10"> */}
+                    <button
+                      // type="submit"
+                      onClick={() =>addCartItem()}
+                      className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      Add to bag
+                    </button>
+                  {/* </form> */}
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  )
+}
